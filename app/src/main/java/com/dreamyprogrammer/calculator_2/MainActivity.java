6d4fc6210a13e358b7eeec4b79d6c52b2335102c
Litvinov Ilya <dreameprogrammer82@gmail.com>
2022-08-03 08:52:38 +0300

    button_click_processing

--- app/src/main/java/com/dreamyprogrammer/calculator_2/MainActivity.java
+++ app/src/main/java/com/dreamyprogrammer/calculator_2/MainActivity.java
@@ -3,14 +3,23 @@ package com.dreamyprogrammer.calculator_2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
+
 public class MainActivity extends AppCompatActivity {
     int[] buttonNumIds;
+    int[] buttonOperationIds;
     View.OnClickListener buttonNumClickListener;
-    TextView textViewFull;
+    View.OnClickListener buttonOperationClickListener;
+    MaterialTextView textViewFull;
+    MaterialButton buttonPercent;
+    MaterialButton buttonParenthesisStart;
+    MaterialButton buttonParenthesisEnd;
+    MaterialButton buttonPoint;
+    MaterialButton buttonEqually;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void findView() {
         textViewFull = findViewById(R.id.text_view_full);
+        buttonPercent = findViewById(R.id.button_percent);
+        buttonParenthesisStart = findViewById(R.id.button_parenthesis_start);
+        buttonParenthesisEnd = findViewById(R.id.button_parenthesis_end);
+        buttonPoint = findViewById(R.id.button_point);
+        buttonEqually = findViewById(R.id.button_equally);
         buttonNumIds = new int[]{
                 R.id.button_1,
                 R.id.button_2,
@@ -34,29 +48,86 @@ public class MainActivity extends AppCompatActivity {
                 R.id.button_9,
                 R.id.button_0
         };
+        buttonOperationIds = new int[]{
+                R.id.button_division,
+                R.id.button_minus,
+                R.id.button_multiplication,
+                R.id.button_plus,
+        };
     }
 
 
     private void setView() {
         buttonNumClickListener = new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                Button button = (Button) v;
+            public void onClick(View view) {
+                Button button = (Button) view;
                 String buttonText = button.getText().toString();
                 textViewFull.setText(textViewFull.getText() + buttonText);
-
-//                calculator.onButtonNumPressed(v.getId());
-//                textViewNotModify.setText(calculator.getText());
-//                if (v.getId() == R.id.button_clean) {
-//                    editTextModify.setText(calculator.getEquallyn());
-//                    textViewNotModify.setText(calculator.getText());
-//                }
+                //TODO Написать обработку цыфр
+            }
+        };
+        buttonOperationClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку операций
             }
         };
 
+        buttonPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку процента
+            }
+        });
+        buttonParenthesisStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку процента
+            }
+        });
+        buttonParenthesisEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку процента
+            }
+        });
+        buttonPoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку процента
+            }
+        });
+        buttonEqually.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String buttonText = button.getText().toString();
+                textViewFull.setText(textViewFull.getText() + buttonText);
+                //TODO Написать обработку процента
+            }
+        });
+
         for (int i = 0; i < buttonNumIds.length; i++) {
             findViewById(buttonNumIds[i]).setOnClickListener(buttonNumClickListener);
+        }
+        for (int i = 0; i < buttonOperationIds.length; i++) {
+            findViewById(buttonOperationIds[i]).setOnClickListener(buttonOperationClickListener);
         }
 
 
